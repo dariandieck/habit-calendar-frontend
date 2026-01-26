bf3ed5b37dec1b621afd57f046f3028cab15b8cf
Darian Dieck <dari.dieck@gmx.de>
2026-01-26 21:25:38 +0100

    logo fix

--- vite.config.ts
+++ vite.config.ts
@@ -23,8 +23,6 @@ export default defineConfig({
         background_color: '#fff',
         theme_color: '#ffb6c1',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
           {
             src: 'apple-touch-icon.png',
             sizes: '180x180',
