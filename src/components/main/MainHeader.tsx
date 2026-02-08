57d6558d168346eb304162645aeb2f249aa32f9d
Darian Dieck <dari.dieck@gmx.de>
2026-02-08 11:42:27 +0100

    changed heading

--- src/components/main/MainHeader.tsx
+++ src/components/main/MainHeader.tsx
@@ -2,7 +2,7 @@ export function MainHeader() {
     return (
         <>
             <h1 className="text-3xl font-bold text-pink-500 mb-8 text-center drop-shadow-sm">
-                ✨ Tägliche Bewertung ✨
+                ✨Tägliche Bewertung✨
             </h1>
             <div className="mb-4">
                 <p className="text-gray-500 leading-relaxed text-md">
